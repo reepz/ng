2c585d56d166e5b972ec89d50f82d7479215fed1
reepz <a.wojtowicz@outlook.com>
2017-07-28 21:29:56 +0200

    task 4 done

---
+++ app/models/concerns/title_brackets_validator.rb
@@ -0,0 +1,32 @@
+class TitleBracketsValidator < ActiveModel::Validator
+  def validate(record)
+
+    # remove everything but the bracket chars
+    stripped = record.title.gsub(/[^\[\]\(\)\{\}]/, '')
+
+    # make an array containing all brackets in title
+    stripped_array = []
+    stripped.each_char do |c|
+      stripped_array << c
+    end
+
+    # array of brackets that have no text inside
+    empty_brackets = ['()', '[]', '{}']
+
+    if stripped.size.odd?
+      record.errors.add(:title, "missing opening or closing bracket(s)")
+    end
+
+
+    if stripped_array[0] == ']'  || stripped_array[0] == ')' || stripped_array[0] == '}'
+      record.errors.add(:title, "incorrect brackets order")
+    end
+
+    empty_brackets.each do |element|
+      if record.title.include?(element)
+        record.errors.add(:title, "empty brackets found")
+      end
+    end
+
+  end
+end
