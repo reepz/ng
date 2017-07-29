7b3588d49050eede1fdf22bc77b2985c96309d31
reepz <a.wojtowicz@outlook.com>
2017-07-29 12:12:48 +0200

    tiny refactor

--- app/models/concerns/title_brackets_validator.rb
+++ app/models/concerns/title_brackets_validator.rb
@@ -4,6 +4,14 @@ class TitleBracketsValidator < ActiveModel::Validator
     # remove everything but the bracket chars
     stripped = record.title.gsub(/[^\[\]\(\)\{\}]/, '')
 
+    if stripped.size.odd?
+      record.errors.add(:title, "missing opening or closing bracket(s)")
+    end
+
+    if stripped_array[0] == ']'  || stripped_array[0] == ')' || stripped_array[0] == '}'
+      record.errors.add(:title, "incorrect brackets order")
+    end
+
     # make an array containing all brackets in title
     stripped_array = []
     stripped.each_char do |c|
@@ -12,15 +20,6 @@ class TitleBracketsValidator < ActiveModel::Validator
 
     # array of brackets that have no text inside
     empty_brackets = ['()', '[]', '{}']
-
-    if stripped.size.odd?
-      record.errors.add(:title, "missing opening or closing bracket(s)")
-    end
-
-
-    if stripped_array[0] == ']'  || stripped_array[0] == ')' || stripped_array[0] == '}'
-      record.errors.add(:title, "incorrect brackets order")
-    end
 
     empty_brackets.each do |element|
       if record.title.include?(element)
